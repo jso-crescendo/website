ec5c57b7aa6930291e5f619b351699232ab7d022
Florian Rohrer <dev@florianrohrer.ch>
2023-10-03 08:54:15 +0200

    take description from concerts array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,14 +62,7 @@ export default function Home() {
             text: 'Weitere Informationen',
           }}
         >
-          Seit 2006 führt das JSO Crescendo regelmässig Chorwerke mit dem
-          Projektchor Canturicum auf. Dieser Chor wurde von Käthi Schmid Lauber
-          eigens dazu gegründet, im Mai 2006 die &quot;Messe solennelle&quot;
-          von Ch. Gounod aufzuführen. Der Erfolg dieses Projektes, u.a. das
-          Konzert in der ausverkauften Tonhalle, war so durchschlagend, dass
-          2007, 2009, 2012, 2015, 2018 und 2019 weitere Projekte realisiert
-          wurden. Es singen jeweils zwischen 80 und 120 Sänger:innen, aus den
-          Singwochen, Eltern, Ehemalige und Freunde vom JSO Crescendo mit.
+          {CONCERTS[0].description}
         </ImageSection>
 
         <ImageSection
