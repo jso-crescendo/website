437fa6d45b943cfb998922a473ad42699fe001b3
Florian Rohrer <dev@florianrohrer.ch>
2024-05-08 14:32:12 +0200

    add etf plakat

--- src/data/concerts/2024/volkstanzgala-24/index.ts
+++ src/data/concerts/2024/volkstanzgala-24/index.ts
@@ -1,15 +1,16 @@
-import Image_sm from './assets/volkstanzgala_sm.webp';
+import Image_sm from './assets/plakat_etf_24_sm.webp';
 
 import {Concert} from '../..';
 
 export const VOLKSTANZGALA_24: Concert = {
   id: 'volkstanzgala-24',
-  name: 'Volkstanzgala',
+  name: 'Eidgenössisches Trachtenfest 2024',
+  subtitle: "Volkstanzgala",
   image_sm: Image_sm,
   dates: [
     {
       location: 'Kongresshaus Zürich',
-      dateString: '29. Juni 2024',
+      dateString: '29. Juni 2024 24:00 Uhr',
       dateISO: '2024-06-29T00:00:00+01:00',
     },
   ],
