f8740ca852e005c3a4fae1ce6837933661bd446a
Florian Rohrer <dev@florianrohrer.ch>
2024-10-27 17:28:02 +0100

    sponsoren gud

--- src/app/unterstuetzen/page.tsx
+++ src/app/unterstuetzen/page.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function Unterstuetzen() {
   return (
-    <ContentContainer title="So können Sie uns unterstützen">
+    <ContentContainer>
       <ImageSection
         image={{src: SponsorenImage, alt: 'Orchestermitglieder beim spielen'}}
         title="Wie finanzieren wir uns?"
@@ -27,7 +27,7 @@ export default function Unterstuetzen() {
 
       <ImageSection
         image={{src: GoennerImage, alt: 'Orchestermitglieder beim spielen'}}
-        title="Gönner"
+        title="Gönner:innen"
         link={{text: 'Jetzt Gönner:in werden', href: '/unterstuetzen/goenner'}}
       >
         Möchten Sie engagierte junge Musiker unterstützen? Mit einem jährlichen
@@ -36,12 +36,48 @@ export default function Unterstuetzen() {
         Vorteile wie Platzreservationen an unseren Konzerten.
       </ImageSection>
       <ImageSection
-        image={{src: SponsorenImage, alt: 'Orchestermitglieder beim spielen'}}
-        title="Sponsoren"
+        image={{
+          src: SponsorenImage,
+          alt: 'Orchestermitglieder beim spielen',
+          vertical: true,
+        }}
+        title="Sponsorenwerbung"
         textOnly={false}
-        link={{text: 'Weitere Informationen', href: '/unterstuetzen/sponsoren'}}
       >
-        TODO
+        <Text>
+          An unseren Konzerten bietet unser Programmheft Platz für Werbung. Ab
+          eimen Betrag von CHF 80 können Sie darin für ihr Unternehmen Werbung
+          machen. Wir drucken jeweils ca. 700 Exemplare und legen dieselbe
+          Edition an allen unseren Konzerten auf.
+        </Text>
+        <div className="my-4">
+          <Text>
+            Wir stellen Ihnen gerne folgende Leistungen zur Verfügung:
+          </Text>
+          <ul className="list-inside list-disc mx-2">
+            <li className="text-justify text-base">Namenserwähnung: CHF 80</li>
+            <li className="text-justify text-base">
+              Inserat A7 (1/4 Seite): CHF 120
+            </li>
+            <li className="text-justify text-base">
+              Inserat A6 (1/2 Seite): CHF 200
+            </li>
+            <li className="text-justify text-base">
+              Inserat A5 (ganze Seite): CHF 400
+            </li>
+          </ul>
+          <Text>Weitere Optionen in Absprache sind gerne möglich.</Text>
+        </div>
+        <Text>
+          Bitte melden Sie sich bei Interesse bis spätestens 4 Wochen vor den
+          Konzerten bei{' '}
+          <EmailLink
+            email="simon.kurath@jso-crescendo.ch"
+            name="Simon Kurath"
+          />
+          . Wir würden uns über Ihre Kontaktaufnahme ausserordentlich freuen —
+          vielen Dank!
+        </Text>
       </ImageSection>
       <ImageSection
         title="Spenden"
@@ -50,8 +86,8 @@ export default function Unterstuetzen() {
         link={{text: 'Einzahlungsschein herunterladen', href: '#TODO'}}
       >
         Wenn sie uns lieber unkompliziert eine einmalige Spende zukommen lassen
-        möchten, können Sie dies jederzeit mit folgendem Einzahlungsschein tun:
-        TODO
+        möchten, können Sie dies jederzeit tun. Auch diese sind herzlich
+        willkommen!
         <section className="pt-4">
           <h3 className="text-xl">Kontodaten</h3>
           <Text className="pl-2">
