d4ebb940150264c79a19a146f8e6936ba36a2d8c
Florian Rohrer <dev@florianrohrer.ch>
2024-10-29 22:24:40 +0100

    suspense

--- src/app/unterstuetzen/goenner/layout.tsx
+++ src/app/unterstuetzen/goenner/layout.tsx
@@ -1,5 +1,7 @@
 import {ContentContainer} from '@/components/contentContainer';
+import {Suspense} from 'react';
 
+// TODO: nice suspense loading spinner
 export default async function Layout(props: {
   children: React.ReactNode;
   form: React.ReactNode;
@@ -8,7 +10,7 @@ export default async function Layout(props: {
     <ContentContainer title="Wollen auch Sie uns unterstützen?">
       {props.children}
       <section className="mt-8 flex flex-row justify-center">
-        {props.form}
+        <Suspense fallback={<div />}>{props.form}</Suspense>
       </section>
     </ContentContainer>
   );
