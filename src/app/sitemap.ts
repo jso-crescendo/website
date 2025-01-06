1f06185dd4c2d4f520dcf91e4379aeea815327c6
Florian Rohrer <dev@florianrohrer.ch>
2025-01-06 22:10:05 +0100

    noellie ausblenden

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
     {
       url: 'https://jso-crescendo.ch/orchester',
-      lastModified: '2024-10-31T22:00:00.000Z',
+      lastModified: '2025-01-06T22:00:00.000Z',
     },
     {
       url: 'https://jso-crescendo.ch/unterstuetzen',
