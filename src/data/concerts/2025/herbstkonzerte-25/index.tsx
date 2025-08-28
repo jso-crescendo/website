6f09aa4f5901301d979dcf985a26aa53533e612f
Florian Rohrer <Florian.Rohrer@swisslife.ch>
2025-08-28 18:22:07 +0200

    Herbstkonzert

--- src/data/concerts/2025/herbstkonzerte-25/index.tsx
+++ src/data/concerts/2025/herbstkonzerte-25/index.tsx
@@ -6,17 +6,27 @@ export const HERBSTKONZERTE_25: Concert = {
   id: 'herbstkonzerte-25',
   name: 'Herbstkonzerte',
   image_sm: Image_sm,
-  lastChange: '2025-05-11T13:00:00+01:00',
-  dates: [
+  lastChange: '2025-08-29T21:00:00+01:00',
+  program: [
     {
-      location: 'Hinwil',
-      dateString: '31. Oktober 2025',
-      dateISO: '2025-10-31T00:00:00+01:00',
+      name: 'Sinfonie No. 9 "Aus der neuen Welt"',
+      composer: 'Antonin Dvorak',
     },
     {
-      location: 'Wird noch bekannt gegeben',
-      dateString: '1. November 2025',
-      dateISO: '2025-11-01T00:00:00+01:00',
+      name: 'Cellokonzert in F-Dur',
+      composer: 'Marie Jaëll',
+    },
+  ],
+  dates: [
+    {
+      location: 'Ref. Kirche Hinwil',
+      dateString: '31. Oktober 2025 20:00 Uhr',
+      dateISO: '2025-10-31T20:00:00+01:00',
+    },
+    {
+      location: 'Ref. Kirche St. Johann Schaffhausen',
+      dateString: '1. November 2025 19:00 Uhr',
+      dateISO: '2025-11-01T19:00:00+01:00',
     },
     {
       location: 'Zürich – Kirche Oberstrass',
