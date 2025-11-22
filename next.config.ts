2f9a16a671948876097bf2dfed3b9929c1d60a3c
Florian Rohrer <dev@florianrohrer.ch>
2025-11-22 20:48:21 +0100

    Update Website

---
+++ next.config.ts
@@ -0,0 +1,93 @@
+import {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/webp'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/goenner',
+        destination: '/unterstuetzen/goenner',
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
+          },
+          {
+            key:
+              process.env.NODE_ENV == 'production'
+                ? 'Content-Security-Policy-Report-Only'
+                : 'X-DEV-CSP',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/:all(.*.pdf)',
+        headers: [
+          {
+            key: 'Content-Disposition',
+            value: 'inline',
+          },
+        ],
+      },
+    ];
+  },
+};
+const ContentSecurityPolicy = `
+  default-src 'self';
+  img-src 'self' data:;
+  script-src 'self' https://analytics.jso-crescendo.ch/ https://challenges.cloudflare.com;
+  script-src-elem 'self' 'unsafe-inline';
+  style-src-attr 'self' 'unsafe-inline';
+  connect-src 'self' https://analytics.jso-crescendo.ch https://vitals.vercel-insights.com/v1/vitals;
+  frame-src 'self' https://challenges.cloudflare.com https://www.youtube-nocookie.com;
+  report-uri https://7b4e0a451e15319a60f0c0a6009f3932.report-uri.com/r/d/csp/wizard;
+  object-src 'none';
+  worker-src 'none';
+`;
+
+module.exports = nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,70 @@
+import './globals.css';
+
+import {Lora, Montserrat} from 'next/font/google';
+
+import {Footer} from '../components/footer';
+import {NavBar} from '@/components/nav-bar';
+import classNames from 'classnames';
+import {Viewport} from 'next';
+
+export const metadata = {
+  title: {template: '%s | JSO Crescendo', default: 'JSO Crescendo'},
+  metadataBase: new URL('https://jso-crescendo.ch'),
+  applicationName: 'JSO Crescendo',
+  keywords: [
+    'orchester',
+    'crescendo',
+    'Jugendsinfonieorchester',
+    'zürich',
+    'orchester mitspielen',
+  ],
+  openGraph: {
+    locale: 'de-CH',
+    type: 'website',
+    siteName: 'JSO Crescendo',
+    url: './',
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#ffffff',
+  colorScheme: 'light',
+};
+
+const loraFont = Lora({
+  variable: '--font-lora',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+});
+const montserratFont = Montserrat({
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+});
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html
+      lang="de-CH"
+      className={classNames(loraFont.variable, montserratFont.variable)}
+    >
+      {process.env.NODE_ENV == 'production' && (
+        <script
+          async
+          defer
+          data-website-id="07350fa3-6016-4694-a802-410a5a7aab98"
+          src="https://analytics.jso-crescendo.ch/script.js"
+        />
+      )}
+      <body className="flex min-h-screen w-screen flex-col overflow-x-hidden bg-background text-on-background">
+        <header className="container mx-auto ">
+          <NavBar />
+        </header>
+        <main className="grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
